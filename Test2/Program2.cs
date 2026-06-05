93c764d19434f19f176b7c38d8a3ba0cf3a6ade2
Phuc Le <phuc05112005@gmail.com>
2026-06-05 13:33:33 +0700

    first commit

---
+++ Test2/Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program2
+{   
+    enum nhietDoNuoc: int
+    {
+        DoDong = 0,
+        DoNguoi = 20,
+        DoAm = 40,
+        DoNong = 60,
+        DoSoi = 100
+    }
+
+    public static void Main2()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        nhietDoNuoc nhietDo = nhietDoNuoc.DoSoi;
+        Console.WriteLine($"Nhiệt độ: {nhietDo}");
+        Console.WriteLine($"Giá trị: {(int)nhietDo}");
+    }
+}
---
+++ Test2/Program6.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program6
+{
+   public static void Main6()
+    {
+        // A. Kich thuoc cach hang giong nhau
+        // Khai bao mang 2 chieu
+        // kieudulieu[,] tenMang;
+        const int rows = 4;
+        const int columns = 3;
+
+        int[,] r = new int[rows, columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                r[i, j] = i + j;
+            }
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                r[i, j] = i + j;
+                Console.Write(r[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("=================");
+
+
+        // B. Kinh thuoc cot trong hang khac nhau
+        int[][] b = new int[rows][];
+
+        b[0] = new int[2];
+        b[1] = new int[3];
+        b[2] = new int[4];
+        b[3] = new int[5];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < b[i].Length; j++)
+            {
+                b[i][j] = i + j;
+                Console.Write(b[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
